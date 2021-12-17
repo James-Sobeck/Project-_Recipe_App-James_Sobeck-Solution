c531ac8feea5ed436b95284ff55d3d6662ebeaef
James Sobeck <sobeckjames@gmail.com>
2021-12-17 12:31:21 -0600

    Updates

---
+++ precache-manifest.0eb2bcdaa7664255faf7e1eaf50235d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb820bec6707255abf70340e1c42447",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "df095b71dcdc15f3df51",
+    "url": "/RecipeApp/static/css/main.6b71d2f8.chunk.css"
+  },
+  {
+    "revision": "f058380acfde14e1f9be",
+    "url": "/RecipeApp/static/js/2.0d2e56b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/RecipeApp/static/js/2.0d2e56b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df095b71dcdc15f3df51",
+    "url": "/RecipeApp/static/js/main.70d82372.chunk.js"
+  },
+  {
+    "revision": "4619c0f0d430dd93d79f",
+    "url": "/RecipeApp/static/js/runtime-main.630e2cf9.js"
+  }
+]);
